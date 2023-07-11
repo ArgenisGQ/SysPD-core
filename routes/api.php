bb79abf86a01b6563b1a29543ee898330cc5f1f2
Argenis Gil <argenis.gq@gmail.com>
2023-07-11 10:22:53 -0400

    Crud de usuarios

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('register', [AuthController::class, 'register']);
+
+Route::post('login', [AuthController::class, 'login']);
+
+//Route::get('logout', [AuthController::class, 'logout']);
+
+Route::middleware(['auth:sanctum'])->group(function(){
+    //rutas protegidas por el auth de sanctum
+    Route::get('logout', [AuthController::class, 'logout']);
 });
